c181223b6b74854295ef5b52b4a4e53d061b9328
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2022-10-29 19:24:05 +0600

    Category CRUD: Category Create form Done

---
+++ resources/views/backend/category/create.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+
+    <title>Create Category!</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1>Create Category!</h1>
+            <form action="">
+               <div class="form-group">
+                 <label for="name">Category Name</label>
+                 <input type="text" class="form-control"  id="name" placeholder=" Category Name">
+               </div>
+              
+              <div class="form-group">
+                <label for="description">Category Description</label>
+                <input type="text" class="form-control" id="description" name="description" placeholder=" Category Description">
+              </div>
+
+              <div class="form-group">
+                <input type="submit" class="btn btn-success" value="Submit">
+              </div>
+            </form>
+         </div>  
+        </div>
+    </div>
+  
+
+ 
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Models\category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories',CategoryController::class);
